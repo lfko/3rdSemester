06ce58478016b92dca045c5b71fa192f135a88f5
lefko <lefko@beauvoir>
2019-10-12 17:55:34 +0200

    (wip) assignment01

--- ImageLearning/Assignments/lfi-01/lfi-01/filter.py
+++ ImageLearning/Assignments/lfi-01/lfi-01/filter.py
@@ -10,9 +10,10 @@ def im2double(im):
     :param im:
     :return: normalized image
     """
-    min_val = np.min(im.ravel())
-    max_val = np.max(im.ravel())
-    out = (im.astype('float') - min_val) / (max_val - min_val)
+    min_val = np.min(im.ravel()) # flatten the array to a 1-d and get the min value
+    max_val = np.max(im.ravel()) # flatten the array to a 1-d and get the max value
+    out = (im.astype('float') - min_val) / (max_val - min_val) 
+    # .astype('float') basically converts the values to floats
     return out
 
 
@@ -22,11 +23,18 @@ def make_gaussian(size, fwhm = 3, center=None):
     size is the length of a side of the square
     fwhm is full-width-half-maximum, which
     can be thought of as an effective radius.
+
+    returns a size-by-size kernel, filled with gaussian created values
+    
+    @NB:- GK is a low-pass filter kernel; good for removing noise. High frequency content (edges, noise) is removed/blurred
+        - 
+        - https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_filtering/py_filtering.html#filtering
     """
 
     x = np.arange(0, size, 1, float)
     y = x[:,np.newaxis]
 
+    # set the center to e.g. x=5, y=5 for size=11
     if center is None:
         x0 = y0 = size // 2
     else:
@@ -44,14 +52,19 @@ def convolution_2d(img, kernel):
     :param img: grayscale image
     :param kernel: convolution matrix - 3x3, or 5x5 matrix
     :return: result of the convolution
+
+    @NB: http://machinelearninguru.com/computer_vision/basics/convolution/image_convolution_1.html
     """
     # TODO write convolution of arbritrary sized convolution here
     # Hint: you need the kernelsize
 
     offset = int(kernel.shape[0]/2)
-    newimg = np.zeros(img.shape)
-
-    # YOUR CODE HERE
+    newimg = np.zeros(img.shape) # picture blank, filled with zeroes, sized liked the original image
+    # TODO ??? Padding ???
+    # iterate pixel by pixel
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            newimg[x, y] = (kernel * img[y:y + 11, x:x + 11]).sum()
 
     return newimg
 
@@ -60,23 +73,33 @@ if __name__ == "__main__":
 
     filename = "Lenna.png"
     # 1. load image in grayscale
-    img = cv2.imread(filename, imread.IMREAD_GRAYSCALE)
-    
-    # 2. convert image to 0-1 image (see im2double)
+    img_gray = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) # (512, 512)
+    img_col = cv2.imread(filename, cv2.IMREAD_COLOR) # (512, 512, 3)
 
+    # cv2.cvtColor does the color conversion from gray to color
+    final_img = np.concatenate((cv2.cvtColor(img_gray,cv2.COLOR_GRAY2RGB, img_col)), axis=1)
+    #print(final_img.shape, final_img.size)
+
+    # 2. convert image to 0-1 image (see im2double)
+    img = im2double(img_gray)
 
     # image kernels
+    # for calculating the gradients
     sobelmask_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
     sobelmask_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
     gk = make_gaussian(11)
-
+    print(gk)
     # 3 .use image kernels on normalized image
+    conv_img = convolution_2d(img, gk)
 
     # 4. compute magnitude of gradients
 
     # Show resulting images
+    '''
     cv2.imshow("sobel_x", sobel_x)
     cv2.imshow("sobel_y", sobel_y)
-    cv2.imshow("mog", mog)
+    '''
+    cv2.imshow("mog", conv_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
--- ImageLearning/Assignments/lfi-01/lfi-01/opencv_experiments.py
+++ ImageLearning/Assignments/lfi-01/lfi-01/opencv_experiments.py
@@ -1,6 +1,21 @@
 import numpy as np
 import cv2
 
+
+'''
+@NB
+    Thresholding helps us in simplifying our picture, e.g. by applying a threshold to a grayscale picture and thus 
+    converting every value below 125 to 0 and above to 1 respectively. For colored images the threshold is applied
+    to every channel independently
+
+@NB
+    Edge Detection
+
+@NB
+    Blurs and Filters: https://www.youtube.com/watch?v=C_zFhWdM4ic&list=PLhhhoPkZzWjZlXDz7AeIl4HfyGMPlsWK7&index=11
+    ED: https://www.youtube.com/watch?v=uihBwtPIBxM&list=PLhhhoPkZzWjZlXDz7AeIl4HfyGMPlsWK7&index=13 (Sobel)
+'''
+
 cap = cv2.VideoCapture(0)
 mode = 0
 while(True):
@@ -10,16 +25,54 @@ while(True):
     # wait for key and switch to mode
     ch = cv2.waitKey(1) & 0xFF
     if ch == ord('1'):
-        mode = 1
-    # ...
+        mode = 1 
+        # HSV
+        hsv, frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+        # look for certain colored objects
+        # define lower and upper thresholds for blue colors
+        lower_blue = np.arrary([110, 50, 50])
+        upper_blue = np.array([130, 255, 255])
+
+        # inRange checks if elements of the array lie between the elements of our boundaries
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)        
+        # use the mask to filter only for blue(ish) objects
+        res = cv2.bitwise_and(frame, frame, mask = mask)
+
+    if ch == ord('2'):
+        mode == 2 
+        # LAB
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+
+    if ch == ord('3'):
+        mode == 3 
+        # YUV
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+    
+    if ch == ord('4'):
+        mode == 4 
+        # Canny Edge Detection (the easy way)
+        frame = cv2.Canny(frame, 100, 200, L2gradient = True)
+
+    
+    if ch == ord('5'):
+        mode == 5 
+        # Thresholding Gaussian, 255 max value, either-or threshold, 50 pixel vicinity for the calculation
+        retval, frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 50, 1)
+    
+    if ch == ord('6'):
+        mode == 6 
+        # Thresholding Otsu, 255 max value, either-or threshold, 50 pixel vicinity for the calculation
+        retval, frame = cv2.threshold(frame, 125, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    
+    
     if ch == ord('q'):
         break
 
-    if mode == 1:
+    #if mode == 1:
         # just example code
         # your code should implement
-        frame = cv2.GaussianBlur(frame, (5, 5), 0)
+        #frame = cv2.GaussianBlur(frame, (5, 5), 0)
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
