e55a83f73a7cefed68618679985ad1666774a418
lefko <lefko@dirichlet.com>
2019-12-12 17:03:12 +0100

    need to repeat that.

---
+++ ImageLearning/svd+pca.py
@@ -0,0 +1,5 @@
+"""
+    SVD and PCA from scratch
+"""
+# https://machinelearningmastery.com/singular-value-decomposition-for-machine-learning/
+# https://machinelearningmastery.com/calculate-principal-component-analysis-scratch-python/
